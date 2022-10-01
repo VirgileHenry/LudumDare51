faad28042585b5bbdb47c237a234c002ee219df4
Eclipse <virgile.henry0211@gmail.com>
2022-10-01 22:52:20 +0200

    refactor planet gen code

--- Scripts/planet/Planet.cs
+++ Scripts/planet/Planet.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Planet
+public class Planet : MonoBehaviour
 {
     PlanetTile[] tiles;
+    public Material planetMaterial; 
 
     // creates a new planet
-    public Planet(int depth, Material material)
+    public void Start()
     {
         Vector3[] icoVerts = new Vector3[12] {
             Vector3.up,
@@ -24,37 +26,38 @@ public class Planet
             -Vector3.up
         };
         float longitude = 0f; // phi
-        float lattitude = Mathf.Atan(0.5f); // theta
+        float latitude = Mathf.Atan(0.5f); // theta
         for(int i = 1; i < 11; i++) {
-            icoVerts[i] = new Vector3(Mathf.Cos(lattitude) * Mathf.Cos(longitude), Mathf.Sin(lattitude), Mathf.Cos(lattitude) * Mathf.Sin(longitude));
+            icoVerts[i] = new Vector3(Mathf.Cos(latitude) * Mathf.Cos(longitude), Mathf.Sin(latitude), Mathf.Cos(latitude) * Mathf.Sin(longitude));
             longitude += 36 * Mathf.PI / 180;
-            lattitude = -lattitude;
+            latitude = -latitude;
         }
 
         tiles = new PlanetTile[20] {
-            new PlanetTile(new TileCoordonates(icoVerts[0], icoVerts[3], icoVerts[1])),
-            new PlanetTile(new TileCoordonates(icoVerts[0], icoVerts[5], icoVerts[3])),
-            new PlanetTile(new TileCoordonates(icoVerts[0], icoVerts[7], icoVerts[5])),
-            new PlanetTile(new TileCoordonates(icoVerts[0], icoVerts[9], icoVerts[7])),
-            new PlanetTile(new TileCoordonates(icoVerts[0], icoVerts[1], icoVerts[9])),
-            new PlanetTile(new TileCoordonates(icoVerts[1], icoVerts[3], icoVerts[2])),
-            new PlanetTile(new TileCoordonates(icoVerts[2], icoVerts[3], icoVerts[4])),
-            new PlanetTile(new TileCoordonates(icoVerts[3], icoVerts[5], icoVerts[4])),
-            new PlanetTile(new TileCoordonates(icoVerts[4], icoVerts[5], icoVerts[6])),
-            new PlanetTile(new TileCoordonates(icoVerts[5], icoVerts[7], icoVerts[6])),
-            new PlanetTile(new TileCoordonates(icoVerts[6], icoVerts[7], icoVerts[8])),
-            new PlanetTile(new TileCoordonates(icoVerts[7], icoVerts[9], icoVerts[8])),
-            new PlanetTile(new TileCoordonates(icoVerts[8], icoVerts[9], icoVerts[10])),
-            new PlanetTile(new TileCoordonates(icoVerts[9], icoVerts[1], icoVerts[10])),
-            new PlanetTile(new TileCoordonates(icoVerts[10], icoVerts[1], icoVerts[2])),
-            new PlanetTile(new TileCoordonates(icoVerts[11], icoVerts[2], icoVerts[4])),
-            new PlanetTile(new TileCoordonates(icoVerts[11], icoVerts[4], icoVerts[6])),
-            new PlanetTile(new TileCoordonates(icoVerts[11], icoVerts[6], icoVerts[8])),
-            new PlanetTile(new TileCoordonates(icoVerts[11], icoVerts[8], icoVerts[10])),
-            new PlanetTile(new TileCoordonates(icoVerts[11], icoVerts[10], icoVerts[2])),
+            new PlanetTile(new Tilecoordinates(icoVerts[0], icoVerts[3], icoVerts[1])),
+            new PlanetTile(new Tilecoordinates(icoVerts[0], icoVerts[5], icoVerts[3])),
+            new PlanetTile(new Tilecoordinates(icoVerts[0], icoVerts[7], icoVerts[5])),
+            new PlanetTile(new Tilecoordinates(icoVerts[0], icoVerts[9], icoVerts[7])),
+            new PlanetTile(new Tilecoordinates(icoVerts[0], icoVerts[1], icoVerts[9])),
+            new PlanetTile(new Tilecoordinates(icoVerts[1], icoVerts[3], icoVerts[2])),
+            new PlanetTile(new Tilecoordinates(icoVerts[2], icoVerts[3], icoVerts[4])),
+            new PlanetTile(new Tilecoordinates(icoVerts[3], icoVerts[5], icoVerts[4])),
+            new PlanetTile(new Tilecoordinates(icoVerts[4], icoVerts[5], icoVerts[6])),
+            new PlanetTile(new Tilecoordinates(icoVerts[5], icoVerts[7], icoVerts[6])),
+            new PlanetTile(new Tilecoordinates(icoVerts[6], icoVerts[7], icoVerts[8])),
+            new PlanetTile(new Tilecoordinates(icoVerts[7], icoVerts[9], icoVerts[8])),
+            new PlanetTile(new Tilecoordinates(icoVerts[8], icoVerts[9], icoVerts[10])),
+            new PlanetTile(new Tilecoordinates(icoVerts[9], icoVerts[1], icoVerts[10])),
+            new PlanetTile(new Tilecoordinates(icoVerts[10], icoVerts[1], icoVerts[2])),
+            new PlanetTile(new Tilecoordinates(icoVerts[11], icoVerts[2], icoVerts[4])),
+            new PlanetTile(new Tilecoordinates(icoVerts[11], icoVerts[4], icoVerts[6])),
+            new PlanetTile(new Tilecoordinates(icoVerts[11], icoVerts[6], icoVerts[8])),
+            new PlanetTile(new Tilecoordinates(icoVerts[11], icoVerts[8], icoVerts[10])),
+            new PlanetTile(new Tilecoordinates(icoVerts[11], icoVerts[10], icoVerts[2])),
         };
 
-        for(int i = 0; i < depth; i++) {
+        // subdivide once
+        for(int i = 0; i < 2; i++) {
             List<PlanetTile> newTiles = new List<PlanetTile>();
             foreach(PlanetTile tile in tiles) {
                 newTiles.AddRange(tile.Subdivide());
@@ -63,19 +66,52 @@ public class Planet
         }
         
         foreach(PlanetTile tile in tiles) {
-            tile.GenerateDebugTriangle(material);
+            tile.GenerateTriangle(planetMaterial);
+        }
+    }
+
+    public void Update() {
+
+    }
+
+    public void UpdateTilesStates(int newFallingTileNumber) {
+        List<PlanetTile> alive = new List<PlanetTile>();
+        foreach(PlanetTile tile in tiles) {
+            switch (tile.state)
+            {
+                case TileState.Alive:
+                    alive.Add(tile);
+                    break;
+                case TileState.Shaking:
+                    tile.Collapse();
+                    break;
+                case TileState.Collapsed:
+                    break;
+            }
+        }
+    }
+
+    private IEnumerator CollapseAnimation(PlanetTile tile, float duration) {
+        float timer = 0;
+        Vector3 oldPos = tile.tileObject.transform.position;
+        Vector3 targetPos = (tile.coordinates.v1 + tile.coordinates.v1 + tile.coordinates.v1).normalized * 0.5f;
+        while(timer < duration) {
+            tile.tileObject.transform.position = oldPos + (timer / duration) * (targetPos - oldPos);
+
+            duration += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
         }
     }
 }
 
 // a position of the tile is represented by 3 corner vertices
-public struct TileCoordonates
+public struct Tilecoordinates
 {
     public Vector3 v1;
     public Vector3 v2;
     public Vector3 v3;
 
-    public TileCoordonates(Vector3 v1, Vector3 v2, Vector3 v3) {
+    public Tilecoordinates(Vector3 v1, Vector3 v2, Vector3 v3) {
         this.v1 = v1;
         this.v2 = v2;
         this.v3 = v3;
@@ -83,7 +119,7 @@ public struct TileCoordonates
 }
 
 // the state of a tile
-enum TileState {
+public enum TileState {
     Alive,
     Shaking,
     Collapsed,
@@ -92,47 +128,75 @@ enum TileState {
 // a tile of a planet
 public class PlanetTile
 {
-    private TileState state;
-    private TileCoordonates coordonates;
+    public TileState state;
+    public Tilecoordinates coordinates;
+    public GameObject tileObject;
 
-    public PlanetTile(TileCoordonates coordonates) {
-        this.coordonates = coordonates;
+    public PlanetTile(Tilecoordinates coordinates) {
+        this.coordinates = coordinates;
         this.state = TileState.Alive;
     }
 
-    // divide the tile in 4 smaller tiles and resturns them
+    // divide the tile in 4 smaller tiles and returns them
     public PlanetTile[] Subdivide() {
-        Vector3 v12 = (coordonates.v1 + coordonates.v2).normalized;
-        Vector3 v13 = (coordonates.v1 + coordonates.v3).normalized;
-        Vector3 v23 = (coordonates.v2 + coordonates.v3).normalized;
+        Vector3 v12 = (coordinates.v1 + coordinates.v2).normalized;
+        Vector3 v13 = (coordinates.v1 + coordinates.v3).normalized;
+        Vector3 v23 = (coordinates.v2 + coordinates.v3).normalized;
 
         return new PlanetTile[4] {
-            new PlanetTile(new TileCoordonates(coordonates.v1, v12, v13)),
-            new PlanetTile(new TileCoordonates(coordonates.v2, v23, v12)),
-            new PlanetTile(new TileCoordonates(coordonates.v3, v13, v23)),
-            new PlanetTile(new TileCoordonates(v12, v23, v13))
+            new PlanetTile(new Tilecoordinates(coordinates.v1, v12, v13)),
+            new PlanetTile(new Tilecoordinates(coordinates.v2, v23, v12)),
+            new PlanetTile(new Tilecoordinates(coordinates.v3, v13, v23)),
+            new PlanetTile(new Tilecoordinates(v12, v23, v13))
         };
     }
 
     public string toString() {
-        return $"{coordonates.v1} {coordonates.v2} {coordonates.v3}";
+        return $"{coordinates.v1} {coordinates.v2} {coordinates.v3}";
     }
 
-    public void GenerateDebugTriangle(Material material) {
-        GameObject go = new GameObject();
-        go.name = "debug triangle";
-        MeshFilter filter = go.AddComponent<MeshFilter>();
-        go.AddComponent<MeshRenderer>().material = material;
+    public void GenerateTriangle(Material material) {
+        tileObject = new GameObject();
+        tileObject.name = "debug triangle";
+        MeshFilter filter = tileObject.AddComponent<MeshFilter>();
+        tileObject.AddComponent<MeshRenderer>().material = material;
         Mesh mesh = new Mesh();
-        mesh.vertices = new Vector3[3] {
-            coordonates.v1,
-            coordonates.v2,
-            coordonates.v3,
+        Vector3 normal = (coordinates.v1 + coordinates.v2 + coordinates.v3).normalized;
+        mesh.vertices = new Vector3[6] {
+            coordinates.v1,
+            coordinates.v2,
+            coordinates.v3,
+            coordinates.v1 * 0.5f,
+            coordinates.v2 * 0.5f,
+            coordinates.v3 * 0.5f,
         };
-        mesh.triangles = new int[3] {
-            0, 1, 2
+        mesh.normals = new Vector3[6] {
+            normal,
+            normal,
+            normal,
+            normal,
+            normal,
+            normal,
+        };
+        mesh.triangles = new int[21] {
+            0, 1, 2,
+            0, 3, 1,
+            3, 4, 1,
+            2, 1, 4,
+            2, 4, 5,
+            0, 2, 3,
+            2, 5, 3
         };
         filter.mesh = mesh;
     }
+
+    public void Collapse() {
+        state = TileState.Collapsed;
+    }
+
+    public void Update() {
+
+    }
+
 
 }
